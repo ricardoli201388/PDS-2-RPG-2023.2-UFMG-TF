698d1ac7b57269d68282ef9f5a09cbd8e5da044e
Filipe <filipehn98@gmail.com>
2023-11-22 18:52:06 -0300

    atualização dos comentários no código

--- src/funcionalidade.cpp
+++ src/funcionalidade.cpp
@@ -1,1 +1,94 @@
 //Lugar onde as funcoes de funcionalidades estarao localizados
+
+//#include <string>
+//#include <unordered_map>
+
+class Arma {
+public:
+    std::string nome;
+    int dano;
+
+    Arma(const std::string& n, int d) : nome(n), dano(d) {}
+};
+
+class WeaponManager {
+private:
+    std::unordered_map<std::string, Arma> armas;
+
+public:
+    WeaponManager() {
+        // Adicionando armas e seus danos ao gerenciador de armas
+        armas.emplace("Machado de cobre", Arma("Machado de cobre", 12));
+        armas.emplace("Machado de ferro", Arma("Machado de ferro", 24));
+        armas.emplace("Machado de ouro", Arma("Machado de ouro", 48));
+
+        armas.emplace("Chave inglesa enferrujada", Arma("Chave inglesa enferrujada", 8));
+        armas.emplace("Chave inglesa polida", Arma("Chave inglesa polida", 10));
+        armas.emplace("Chave inglesa reluzente", Arma("Chave inglesa reluzente", 20));
+
+        armas.emplace("Rapieira de Pedra", Arma("Rapieira de Pedra", 8));
+        armas.emplace("Rapieira de Quartzo", Arma("Rapieira de Quartzo", 10));
+        armas.emplace("Rapieira de Obsidiana", Arma("Rapieira de Obsidiana", 20));
+    }
+
+    // Função para obter uma arma pelo nome
+    Arma getArma(const std::string& nomeArma) {
+        if (armas.find(nomeArma) != armas.end()) {
+            return armas[nomeArma];
+        } else {
+            // Caso a arma não exista, retornar uma arma padrão (nome vazio e dano 0)
+            return Arma("", 0);
+        }
+    }
+};
+
+
+
+
+
+class Magia {
+public:
+    std::string nome;
+    int dano;
+
+    Magia(const std::string& n, int d) : nome(n), dano(d) {}
+};
+
+class Gerenciador_Magia {
+private:
+    std::unordered_map<std::string, Magia> magias;
+
+public:
+    Gerenciador_Magia() {
+        // Inventor
+        magias.emplace("Raio Chocante", Magia("Raio Chocante", 6));
+        magias.emplace("Algoritmo ancestral", Magia("Algoritmo ancestral", 24));
+
+        // Druida
+        magias.emplace("Raio Ácido", Magia("Raio Ácido", 6));
+
+        // Mago
+        magias.emplace("Projétil Verde", Magia("Projétil Verde", 8));
+        magias.emplace("Raio de Gelo", Magia("Raio de Gelo", 6));
+        magias.emplace("Ventos uivantes", Magia("Ventos uivantes", 10));
+        magias.emplace("Choque Avassalador", Magia("Choque Avassalador", 16));
+        magias.emplace("Bola de Fogo", Magia("Bola de Fogo", 48));
+
+        // Bardo
+        magias.emplace("Notas falhas", Magia("Notas falhas", 6));
+        magias.emplace("Poema ofensivo", Magia("Poema ofensivo", 8));
+        magias.emplace("Spray colorido", Magia("Spray colorido", 10));
+        magias.emplace("Canção do Terror", Magia("Canção do Terror", 16));
+        magias.emplace("Dança Forçada", Magia("Dança Forçada", 48));
+    }
+
+    // Função para obter uma magia pelo nome
+    Magia getMagia(const std::string& nomeMagia) {
+        if (magias.find(nomeMagia) != magias.end()) {
+            return magias[nomeMagia];
+        } else {
+            // Caso a magia não exista, retornar uma magia padrão (nome vazio e dano 0)
+            return Magia("", 0);
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 
 using namespace std;
 
+// Definição da classe Player para representar o personagem do jogador
 class Player {
 public:
     string name;
@@ -25,32 +26,40 @@ public:
     int level;
     bool defending;
 
+
+// Construtor da classe Player para inicializar seus atributos
     Player(string n, int h, int a) : name(n), health(h), attack(a), potionCount(3), level(1), defending(false) {}
 };
 
+// Definição da classe Enemy para representar os inimigos
 class Enemy {
 public:
     string name;
     int health;
     int attack;
 
+// Construtor da classe Enemy para inicializar seus atributos
     Enemy(string n, int h, int a) : name(n), health(h), attack(a) {}
 };
 
+// Função para retornar um valor booleano aleatório baseado em uma probabilidade
 bool getRandomBool(double probability) {
     return (rand() / static_cast<double>(RAND_MAX)) < probability;
 }
 
+// Função para imprimir o status do jogador e do inimigo na tela
 void printStatus(Player& player, Enemy& enemy) {
-    clear();
+    clear(); // Limpa a tela
 
-    // Imprime a caixa de menu
+    // Imprime uma caixa de menu com as informações de saúde (HP) do jogador e do inimigo
     box(stdscr, 0, 0);
     mvprintw(1, 1, " Jogo RPG - HP de %s: %d  HP de %s: %d", player.name.c_str(), player.health, enemy.name.c_str(), enemy.health);
 
     // Imprime o avatar do herói
     mvprintw(3, 1, "Herói: ");
     attron(A_BOLD);
+    
+    // Desenha o avatar do herói
     mvprintw(3, 7, R"(
    ,   A           {}
   / \, | ,        .--.
@@ -65,109 +74,116 @@ void printStatus(Player& player, Enemy& enemy) {
        | `""`  /   ^^  |   ':'   |
     )"); 
 
-    attroff(A_BOLD);
+    attroff(A_BOLD); // Desativa o atributo de negrito
 
-    // Imprime o conteúdo do jogo
+     // Imprime a mensagem para escolha de ação
     mvprintw(14, 1, "Escolha uma ação (a = atacar, d = defender, u = usar poção): ");
     refresh();
 }
 
+// Função para obter o nome do jogador
 void getPlayerName(string& playerName) {
-    mvprintw(8, 1, "Digite o nome do jogador: ");
-    refresh();
+    mvprintw(8, 1, "Digite o nome do jogador: ");// Imprime a mensagem para digitar o nome do jogador
+    refresh();// Atualiza a tela
     char buffer[50];
     int i = 0;
+    
     while (true) {
-        int ch = getch();
+        int ch = getch(); // Obtém o próximo caractere digitado pelo usuário
 
         if (ch == '\n' || ch == '\r') {
-            buffer[i] = '\0';
+            buffer[i] = '\0'; // Termina a string com null character
             break;
         } else if (ch == 127 && i > 0) {
-            buffer[--i] = '\0';
+            buffer[--i] = '\0'; // Remove o último caractere digitado (tecla backspace)
         } else if (i < 49) {
-            buffer[i++] = ch;
-            buffer[i] = '\0';
+            buffer[i++] = ch;// Adiciona o caractere ao buffer
+            buffer[i] = '\0';// Termina a string com null character
         }
 
-        clear();
+        clear(); // Limpa a tela
         // Imprime a caixa de menu
-        box(stdscr, 0, 0);
-        mvprintw(1, 1, " Jogo RPG - Digite o nome do jogador: %s", buffer);
-        refresh();
+        box(stdscr, 0, 0); // Desenha a caixa de menu
+        mvprintw(1, 1, " Jogo RPG - Digite o nome do jogador: %s", buffer); // Imprime o nome digitado
+        refresh();// Atualiza a tela
     }
-    playerName = buffer;
+    playerName = buffer;  // Atualiza o nome do jogador com o valor digitado
 }
 
+// Função que representa a vez do jogador
 void playerTurn(Player& player, Enemy& enemy) {
-    printStatus(player, enemy);
+    printStatus(player, enemy); // Mostra o status do jogador e inimigo na tela
 
-    char action = getch();
+    char action = getch(); // Obtém a ação escolhida pelo jogador
 
-    if (action == 'a') {
-        int dano = player.attack;
-        if (getRandomBool(0.2)) {
-            dano *= 2;
-            mvprintw(16, 1, "Golpe Crítico! Você atacou %s e causou %d de dano!", enemy.name.c_str(), dano);
+    // Verifica a ação escolhida e executa a ação correspondente
+    if (action == 'a') {// Ataque
+        int dano = player.attack;   // Dano base do jogador
+        if (getRandomBool(0.2)) {   // Verifica se houve um golpe crítico (20% de chance)
+            dano *= 2;              // Dano dobrado em caso de golpe crítico
+            mvprintw(16, 1, "Golpe Crítico! Você atacou %s e causou %d de dano!", enemy.name.c_str(), dano); // Exibe mensagem de golpe crítico
         } else {
-            enemy.health -= dano;
+            enemy.health -= dano;// Reduz a saúde do inimigo pelo dano
             mvprintw(16, 1, "Você atacou %s e causou %d de dano!", enemy.name.c_str(), dano);
         }
-    } else if (action == 'd') {
-        player.defending = true;
+    } else if (action == 'd') {// Defesa
+        player.defending = true;// Ativa a defesa do jogador
         mvprintw(16, 1, "Você está se defendendo dos ataques de %s.", enemy.name.c_str());
-    } else if (action == 'u') {
-        if (player.potionCount > 0) {
-            int cura = 50;
-            player.health += cura;
-            player.potionCount--;
-            mvprintw(16, 1, "Você usou uma poção e recuperou %d de HP. Poções restantes: %d", cura, player.potionCount);
+    } else if (action == 'u') { // Uso de poção
+        if (player.potionCount > 0) { 
+            int cura = 50; // Quantidade de cura da poção
+            player.health += cura; // Aumenta a saúde do jogador
+            player.potionCount--; // Decrementa o contador de poções
+            mvprintw(16, 1, "Você usou uma poção e recuperou %d de HP. Poções restantes: %d", cura, player.potionCount);// Exibe mensagem de uso de poção
         } else {
-            mvprintw(16, 1, "Você não tem mais poções!");
+            mvprintw(16, 1, "Você não tem mais poções!"); // Exibe mensagem se não houver poções disponíveis
         }
     } else {
-        mvprintw(16, 1, "Ação inválida. Tente novamente.");
+        mvprintw(16, 1, "Ação inválida. Tente novamente."); // Mensagem para ação inválida
         refresh();
         return;
     }
-    mvprintw(17, 1, "Pressione qualquer tecla para continuar...");
-    getch();
-    refresh();
+    mvprintw(17, 1, "Pressione qualquer tecla para continuar...");// Mensagem para continuar
+    getch();// Aguarda entrada do usuário
+    refresh();// Atualiza a tela
 }
-
+// Função que representa a vez do inimigo
 void enemyTurn(Player& player, Enemy& enemy) {
-    if (player.health > 0) {
-        int dano = enemy.attack;
-        if (player.defending) {
-            dano /= 2;
+    if (player.health > 0) {  // Verifica se o jogador ainda está vivo
+        int dano = enemy.attack; // Dano base do inimigo
+        if (player.defending) { // Verifica se o jogador está defendendo
+            dano /= 2; // Reduz o dano pela metade se o jogador está defendendo
         }
-        player.health -= dano;
-        mvprintw(18, 1, "%s atacou você e causou %d de dano!", enemy.name.c_str(), dano);
-        mvprintw(19, 1, "Pressione qualquer tecla para continuar...");
-        getch();
+        player.health -= dano; // Reduz a saúde do jogador pelo dano do inimigo
+        mvprintw(18, 1, "%s atacou você e causou %d de dano!", enemy.name.c_str(), dano);// Exibe mensagem de ataque
+        mvprintw(19, 1, "Pressione qualquer tecla para continuar...");// Mensagem para continuar
+        getch();// Aguarda entrada do usuário
     }
-    refresh();
+    refresh();// Atualiza a tela
 }
 
+
+// Função para evoluir o jogador de nível
 void levelUp(Player& player) {
-    player.level++;
-    player.attack += 5;
-    mvprintw(16, 1, "Parabéns! Você subiu para o nível %d!", player.level);
-    mvprintw(17, 1, "Pressione qualquer tecla para continuar...");
-    getch();
-    refresh();
+    player.level++;// Incrementa o nível do jogador
+    player.attack += 5; // Aumenta o poder de ataque do jogador
+    mvprintw(16, 1, "Parabéns! Você subiu para o nível %d!", player.level);// Exibe mensagem de evolução de nível
+    mvprintw(17, 1, "Pressione qualquer tecla para continuar...");// Mensagem para continuar
+    getch();// Aguarda entrada do usuário
+    refresh();// Atualiza a tela
 }
 
+// Função que representa a batalha entre o jogador e o inimigo
 void battle(Player& player, Enemy& enemy) {
-    mvprintw(16, 1, "Você está em batalha com %s!", enemy.name.c_str());
-    refresh();
+    mvprintw(16, 1, "Você está em batalha com %s!", enemy.name.c_str());// Mensagem de início de batalha
+    refresh();// Atualiza a tela
 
-    while (player.health > 0 && enemy.health > 0) {
-        playerTurn(player, enemy);
-        if (enemy.health <= 0) {
-            mvprintw(16, 1, "Você derrotou %s. Parabéns!", enemy.name.c_str());
+    while (player.health > 0 && enemy.health > 0) { // Enquanto ambos (jogador e inimigo) estiverem vivos
+        playerTurn(player, enemy);// Vez do jogador
+        if (enemy.health <= 0) {// Se o inimigo estiver derrotado
+            mvprintw(16, 1, "Você derrotou %s. Parabéns!", enemy.name.c_str());  // Mensagem de vitória
             if (player.health > 0 && player.level < 3) {
-                levelUp(player);
+                levelUp(player); // Evolui o jogador se ainda não atingiu o nível máximo
             }
             break;
         }
@@ -183,26 +199,37 @@ void battle(Player& player, Enemy& enemy) {
 
 
 int main() {
+    
+    // Inicialização do ambiente ncurses
     initscr();
     raw();
     keypad(stdscr, TRUE);
     noecho();
 
+// Mensagem de início do jogo
     mvprintw(1, 1, " RPG Game - Torre UFMG!");
 
+// Obter o nome do jogador
     string playerName;
     getPlayerName(playerName);
 
+// Inicialização do jogador e inimigo
     Player player(playerName, 200, 20);
     Enemy enemy("Enemy", 80, 15);
 
+// Mensagem de entrada no mundo de aventuras
     mvprintw(3, 1, "Voce entrou num mundo de aventuras!");
 
+// Semente para gerar números aleatórios
     srand(time(0));
 
+// Loop principal do jogo enquanto o jogador está vivo e não atingiu o nível máximo
     while (player.health > 0 && player.level < 3) {
+         // Lógica das batalhas
         battle(player, enemy);
 
+
+// Lógica para preparar para a próxima batalha
         if (player.health > 0 && player.level < 3) {
             mvprintw(15, 1, "Voce prepara para proxima batalha!");
             refresh();
@@ -211,13 +238,16 @@ int main() {
             int randomEnemyHealth = rand() % 100 + 50;
             int randomEnemyAttack = rand() % 20 + 10;
 
+// Geração aleatória do inimigo para a próxima batalha
             enemy = Enemy(randomEnemyName, randomEnemyHealth, randomEnemyAttack);
 
+// Mensagem sobre o encontro do jogador com o novo inimigo
             mvprintw(15, 1, "Voce encontra um %s!", enemy.name.c_str());
             refresh();
         }
     }
 
+// Mensagem final do jogo dependendo do resultado
     if (player.level == 3) {
         mvprintw(16, 1, "Parabens! Voce atingiu o nivel maximo e completou o jogo!");
     } else {
